9f387d88ef250d8cbd11def38507f1fb753e0732
mahdi0shamlou <shamlouprogramer@gmail.com>
2026-02-21 14:46:19 -0500

    refactor(k6-tests/basic): update how to test many case

--- k6-tests/basic.js
+++ k6-tests/basic.js
@@ -1,5 +1,22 @@
-import http from 'k6/http';
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export const options = {
+  vus: 50,          // number of users
+  duration: "30s",  // test time
+};
+
+const targets = [
+  { name: "fastapi", url: "http://localhost:8001/hello" },
+  { name: "flask",   url: "http://localhost:8002/hello" },
+];
 
 export default function () {
-  http.get('http://localhost:8001/hello');
+  for (const t of targets) {
+    const res = http.get(t.url);
+    check(res, {
+      [`${t.name} status 200`]: (r) => r.status === 200,
+    });
+  }
+  sleep(1);
 }
